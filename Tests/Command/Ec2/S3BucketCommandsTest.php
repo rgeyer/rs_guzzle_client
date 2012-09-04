12b129a601db00ab5f9f11320e3e449bfb4e7166
Ryan J. Geyer <me@ryangeyer.com>
2012-09-04 15:01:19 -0700

    Refactored Ec2\S3BucketCommandsTest to proper unit tests

--- Tests/Command/Ec2/S3BucketCommandsTest.php
+++ Tests/Command/Ec2/S3BucketCommandsTest.php
@@ -1,32 +1,155 @@
 <?php
 namespace RGeyer\Guzzle\Rs\Tests\Command\Ec2;
 
-use RGeyer\Guzzle\Rs\Tests\Utils\ClientCommandsBase;
+use RGeyer\Guzzle\Rs\Common\ClientFactory;
 
-class S3BucketCommandsTest extends ClientCommandsBase {
-	
-	/**
-	 * @group v1_0
-	 * @group integration
-	 */
-	public function testCanCreateS3Bucket() {
-		$command = null;
-		$result = $this->executeCommand('s3_buckets_create', array('s3_bucket[name]' => 'RS_Guzzle_Tests_' . $this->_testTs), $command);
-		$this->assertEquals(201, $command->getResponse()->getStatusCode());
-		$this->assertNotNull($command->getResponse()->getHeader('Location'));
-		
-		return $this->getIdFromHref('s3_buckets', $command->getResponse()->getHeader('Location'));
-	}
-	
-	/**
-	 * @group v1_0
-	 * @group integration
-	 * @depends testCanCreateS3Bucket
-	 */
-	public function testCanDestroyS3Bucket($bucketId) {
-		$command = null;
-		$result = $this->executeCommand('s3_buckets_destroy', array('id' => $bucketId), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());
-	}
+class S3BucketCommandsTest extends \Guzzle\Tests\GuzzleTestCase {
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testHasDestroyCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('s3_buckets_destroy');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testDestroyUsesCorrectVerb() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/s3_buckets_destroy/response'
+      )
+    );
+
+    $command = $client->getCommand('s3_buckets_destroy',array('id' => 1234));
+    $command->execute();
+
+    $this->assertEquals('DELETE', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testDestroyCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('s3_buckets_destroy');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage  id argument be supplied.
+   */
+  public function testDestroyRequiresId() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/s3_buckets_destroy/response'
+      )
+    );
+
+    $command = $client->getCommand('s3_buckets_destroy');
+    $command->execute();
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage id: Value must be numeric
+   */
+  public function testDestroyRequiresIdToBeAnInt() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/s3_buckets_destroy/response'
+      )
+    );
+
+    $command = $client->getCommand('s3_buckets_destroy', array('id' => 'abc'));
+    $command->execute();
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testHasCreateCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('s3_buckets_create');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testCreateUsesCorrectVerb() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/s3_buckets_create/response'
+      )
+    );
+
+    $command = $client->getCommand('s3_buckets_create',
+      array(
+        's3_bucket[name]' => 'name'
+      )
+    );
+    $command->execute();
+
+    $this->assertEquals('POST', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testCreateCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('s3_buckets_create');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage s3_bucket[name] argument be supplied.
+   */
+  public function testCreateRequiresName() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/s3_buckets_create/response'
+      )
+    );
+
+    $command = $client->getCommand('s3_buckets_create');
+    $command->execute();
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testCreateCommandReturnsAModel() {
+    $this->markTestSkipped("A model does not yet exist");
+  }
 	
 }
