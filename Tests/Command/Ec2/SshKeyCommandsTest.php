06820c187478820ded55f6a05ea7cff25d744a3f
Ryan J. Geyer <me@ryangeyer.com>
2012-09-11 11:52:26 -0700

    Refactored Ec2\SshKeyCommandsTest to proper unit tests

--- Tests/Command/Ec2/SshKeyCommandsTest.php
+++ Tests/Command/Ec2/SshKeyCommandsTest.php
@@ -2,36 +2,329 @@
 
 namespace RGeyer\Guzzle\Rs\Tests\Command\Ec2;
 
-use RGeyer\Guzzle\Rs\Tests\Utils\ClientCommandsBase;
+use RGeyer\Guzzle\Rs\Common\ClientFactory;
 
-class SshKeyCommandsTest extends ClientCommandsBase {
-	
-	protected $_ssh_key_id;
-	
-	protected function setUp() {
-		parent::setUp();
-		
-		$key = $this->executeCommand('ec2_ssh_keys_create', array('ec2_ssh_key[aws_key_name]' => "Guzzle_Test_$this->_testTs"));
-		
-		$this->_ssh_key_id = $key->id;
-	}
-	
-	protected function tearDown() {		
-		$this->executeCommand('ec2_ssh_keys_destroy', array('id' => $this->_ssh_key_id));
+class SshKeyCommandsTest extends \Guzzle\Tests\GuzzleTestCase {
 
-		parent::tearDown();
-	}
-	
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testHasShowCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('ec2_ssh_key');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testShowUsesCorrectVerb() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/ec2_ssh_key/js/response'
+      )
+    );
+
+    $command = $client->getCommand('ec2_ssh_key', array('id' => 1234));
+    $command->execute();
+
+    $this->assertEquals('GET', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testShowCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('ec2_ssh_key');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage  id argument be supplied.
+   */
+  public function testShowRequiresId() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/ec2_ssh_key/js/response'
+      )
+    );
+
+    $command = $client->getCommand('ec2_ssh_key');
+    $command->execute();
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage id: Value must be numeric
+   */
+  public function testShowRequiresIdToBeAnInt() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/ec2_ssh_key/js/response'
+      )
+    );
+
+    $command = $client->getCommand('ec2_ssh_key', array('id' => 'abc'));
+    $command->execute();
+  }
+
 	/**
 	 * @group v1_0
-	 * @group integration
+	 * @group unit
 	 */
-	public function testCanShowAKey() {
-		$result = $this->executeCommand('ec2_ssh_key', array('id' => $this->_ssh_key_id));
-				
-		$this->assertEquals("Guzzle_Test_$this->_testTs", $result->aws_key_name);
+	public function testShowDefaultsOutputTypeToJson() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/ec2_ssh_key/js/response'
+      )
+    );
+
+    $command = $client->getCommand('ec2_ssh_key', array('id' => 1234));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/ec2_ssh_keys/1234.js', $request);
 	}
-	
+
+	/**
+	 * @group v1_0
+	 * @group unit
+	 */
+	public function testShowAsJson() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/ec2_ssh_key/js/response'
+      )
+    );
+
+    $command = $client->getCommand('ec2_ssh_key', array('id' => 1234, 'output_format' => '.js'));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/ec2_ssh_keys/1234.js', $request);
+	}
+
+	/**
+	 * @group v1_0
+	 * @group unit
+	 */
+	public function testShowAsXml() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/ec2_ssh_key/xml/response'
+      )
+    );
+
+    $command = $client->getCommand('ec2_ssh_key', array('id' => 1234, 'output_format' => '.xml'));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/ec2_ssh_keys/1234.xml', $request);
+	}
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testShowCommandReturnsAModel() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/ec2_ssh_key/js/response'
+      )
+    );
+
+    $command = $client->getCommand('ec2_ssh_key', array('id' => '12345'));
+    $command->execute();
+    $result = $command->getResult();
+
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Model\Ec2\SshKey', $result);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testHasCreateCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('ec2_ssh_keys_create');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testCreateUsesCorrectVerb() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/ec2_ssh_keys_create/response'
+      )
+    );
+
+    $command = $client->getCommand('ec2_ssh_keys_create',
+      array(
+        'ec2_ssh_key[aws_key_name]' => 'name'
+      )
+    );
+    $command->execute();
+
+    $this->assertEquals('POST', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testCreateCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('ec2_ssh_keys_create');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage ec2_ssh_key[aws_key_name] argument be supplied.
+   */
+  public function testCreateRequiresNickname() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/ec2_ssh_keys_create/response'
+      )
+    );
+
+    $command = $client->getCommand('ec2_ssh_keys_create');
+    $command->execute();
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testCreateCommandReturnsAModel() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/ec2_ssh_keys_create/response'
+      )
+    );
+
+    $command = $client->getCommand('ec2_ssh_keys_create',
+      array(
+        'ec2_ssh_key[aws_key_name]' => 'name'
+      )
+    );
+    $command->execute();
+    $result = $command->getResult();
+
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Model\Ec2\SshKey', $result);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testHasDestroyCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('ec2_ssh_keys_destroy');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testDestroyUsesCorrectVerb() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/ec2_ssh_keys_destroy/response'
+      )
+    );
+
+    $command = $client->getCommand('ec2_ssh_keys_destroy',array('id' => 1234));
+    $command->execute();
+
+    $this->assertEquals('DELETE', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testDestroyCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('ec2_ssh_keys_destroy');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage  id argument be supplied.
+   */
+  public function testDestroyRequiresId() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/ec2_ssh_keys_destroy/response'
+      )
+    );
+
+    $command = $client->getCommand('ec2_ssh_keys_destroy');
+    $command->execute();
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage id: Value must be numeric
+   */
+  public function testDestroyRequiresIdToBeAnInt() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/ec2_ssh_keys_destroy/response'
+      )
+    );
+
+    $command = $client->getCommand('ec2_ssh_keys_destroy', array('id' => 'abc'));
+    $command->execute();
+  }
 }
 
 ?>
