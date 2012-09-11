8caa914dcc75dc9cc6582ee27a78670a5187c3e0
Ryan J. Geyer <me@ryangeyer.com>
2012-09-11 12:21:38 -0700

    Refactored Ec2\TagCommandsTest to proper unit tests

--- Tests/Command/Ec2/TagCommandsTest.php
+++ Tests/Command/Ec2/TagCommandsTest.php
@@ -2,215 +2,386 @@
 
 namespace RGeyer\Guzzle\Rs\Tests\Command\Ec2;
 
-use RGeyer\Guzzle\Rs\Model\Ec2\Deployment;
+use RGeyer\Guzzle\Rs\Common\ClientFactory;
 
-use RGeyer\Guzzle\Rs\Tests\Utils\ClientCommandsBase;
+class TagCommandsTest extends \Guzzle\Tests\GuzzleTestCase {
 
-class TagCommandsTest extends ClientCommandsBase {
-		
-	protected function setUp() {
-		parent::setUp();
-	}
-	
-	protected function tearDown() {
-		parent::tearDown();
-	}
-	
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testHasTaggableResourcesCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('tags_taggable_resources');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testTaggableResourcesUsesCorrectVerb() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_taggable_resources/js/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_taggable_resources');
+    $command->execute();
+
+    $this->assertEquals('GET', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testTaggableResourcesCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('tags_taggable_resources');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
 	/**
 	 * @group v1_0
-	 * @group integration
+	 * @group unit
 	 */
-	public function testCanListTaggableResourcesJson() {
-		$result = $this->executeCommand('tags_taggable_resources');
-		
-		$json_obj = json_decode($result->getBody(true));
-		
-		$expected_resource_list = array("Server","Ec2EbsSnapshot","Ec2EbsVolume","Ec2Image","Image","ServerArray","Ec2Instance","Instance","Deployment","ServerTemplate","MultiCloudImage");
-		
-		$this->assertEquals(11, count($json_obj));
-		$this->assertEquals($expected_resource_list, $json_obj);
-		$this->assertEquals(200, $result->getStatusCode());
+	public function testTaggableResourcesDefaultsOutputTypeToJson() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_taggable_resources/js/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_taggable_resources');
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/tags/taggable_resources.js', $request);
 	}
-	
+
 	/**
 	 * @group v1_0
-	 * @group integration
+	 * @group unit
 	 */
-	public function testCanListTaggableResourcesXml() {
-		$command = null;
-		$result = $this->executeCommand('tags_taggable_resources', array('output_format' => '.xml'), $command);
-						
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());
-		$this->assertInstanceOf('SimpleXMLElement', $result);
-		$this->assertEquals(11, count($result));		
+	public function testCanRequestTaggableResourcesAsJson() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_taggable_resources/js/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_taggable_resources', array('output_format' => '.js'));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/tags/taggable_resources.js', $request);
 	}
-	
+
 	/**
 	 * @group v1_0
-	 * @group integration
+	 * @group unit
 	 */
-	public function testCanSetAndFindOneTag() {
-		$deployment = new Deployment();
-		$deployment->nickname = "Guzzle_Test_For_Tag_" . $this->_testTs;
-		$deployment->description = 'described';
-		$deployment->create();
-		
-		$result = $this->executeCommand('tags_set', array('resource_href' => $deployment->href, 'tags' => array('rs_guzzle_client:tagged=true')));		
-		$this->assertEquals(204, $result->getStatusCode());
-		
-		$command = null;
-		$result = $this->executeCommand('tags_search', array('resource_href' => $deployment->href), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());		
-		$json_obj = json_decode($result->getBody(true));
-		
-		$this->assertEquals(1, count($json_obj));
-		$this->assertEquals('rs_guzzle_client:tagged=true', $json_obj[0]->name);
-		
-		return $deployment;
+	public function testCanRequestTaggableResourcesAsXml() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_taggable_resources/xml/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_taggable_resources', array('output_format' => '.xml'));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/tags/taggable_resources.xml', $request);
 	}
-	
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testHasTagsSetCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('tags_set');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testTagsSetUsesCorrectVerb() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_set/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_set',
+      array(
+        'resource_href' => 'https://my.rightscale.com/api/acct/12345/ec2_server_templates/12345',
+        'tags' => array('ns:predicate=true')
+      )
+    );
+    $command->execute();
+
+    $this->assertEquals('PUT', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testTagsSetCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('tags_set');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage resource_href argument be supplied.
+   */
+  public function testTagsSetRequiresResourceHref() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_set/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_set',
+      array(
+        'tags' => array('ns:predicate=true')
+      )
+    );
+    $command->execute();
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage tags argument be supplied.
+   */
+  public function testTagsSetRequiresTags() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_set/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_set',
+      array(
+        'resource_href' => 'https://my.rightscale.com/api/acct/12345/ec2_server_templates/12345'
+      )
+    );
+    $command->execute();
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testHasTagsUnsetCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('tags_unset');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testTagsUnsetUsesCorrectVerb() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_unset/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_unset',
+      array(
+        'resource_href' => 'https://my.rightscale.com/api/acct/12345/ec2_server_templates/12345',
+        'tags' => array('ns:predicate=true')
+      )
+    );
+    $command->execute();
+
+    $this->assertEquals('PUT', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testTagsUnsetCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('tags_unset');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage resource_href argument be supplied.
+   */
+  public function testTagsUnsetRequiresResourceHref() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_unset/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_unset',
+      array(
+        'tags' => array('ns:predicate=true')
+      )
+    );
+    $command->execute();
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage tags argument be supplied.
+   */
+  public function testTagsUnsetRequiresTags() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_unset/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_unset',
+      array(
+        'resource_href' => 'https://my.rightscale.com/api/acct/12345/ec2_server_templates/12345'
+      )
+    );
+    $command->execute();
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testHasSearchCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('tags_search');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testSearchUsesCorrectVerb() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_search/js/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_search');
+    $command->execute();
+
+    $this->assertEquals('GET', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testSearchCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('tags_search');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
 	/**
 	 * @group v1_0
-	 * @group integration
-	 * @depends testCanSetAndFindOneTag
+	 * @group unit
 	 */
-	public function testCanUnsetOneTag($deployment) {
-		$result = $this->executeCommand('tags_unset', array('resource_href' => $deployment->href, 'tags' => array('rs_guzzle_client:tagged=true')));
-		
-		$this->assertEquals(204, $result->getStatusCode());
-		
-		$command = null;
-		$result = $this->executeCommand('tags_search', array('resource_href' => $deployment->href), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());		
-		$json_obj = json_decode($result->getBody(true));
-		
-		$this->assertEquals(0, count($json_obj));
-		
-		$deployment->destroy();
+	public function testSearchDefaultsOutputTypeToJson() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_search/js/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_search');
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/tags/search.js', $request);
 	}
-	
+
 	/**
 	 * @group v1_0
-	 * @group integration
+	 * @group unit
 	 */
-	public function testCanSearchWithAllParamsJson() {		
-		$deployment = new Deployment();
-		$deployment->nickname = "Guzzle_Test_For_Tag_" . $this->_testTs;
-		$deployment->description = 'described';
-		$deployment->create();
-		
-		# Check for 0 tags first
-		$command = null;
-		$result = $this->executeCommand('tags_search', array('resource_href' => $deployment->href), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());
-		$json_obj = json_decode($result->getBody(true));
-		$this->assertEquals(0, count($json_obj));
-		
-		# Set one tag, then find it, this is duplicated above, but let's do it here for good measure
-		$result = $this->executeCommand('tags_set', array('resource_href' => $deployment->href, 'tags' => array('rs_guzzle_client:tag1=true')));
-		$this->assertEquals(204, $result->getStatusCode());
-		$command = null;
-		$result = $this->executeCommand('tags_search', array('resource_href' => $deployment->href), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());
-		$json_obj = json_decode($result->getBody(true));
-		$this->assertEquals(1, count($json_obj));
-		
-		# Set another tag, used for additional tests later
-		$result = $this->executeCommand('tags_set', array('resource_href' => $deployment->href, 'tags' => array('rs_guzzle_client:tag2=true')));
-		$this->assertEquals(204, $result->getStatusCode());
-		
-		# Search by deployment resource type
-		$command = null;
-		$result = $this->executeCommand('tags_search', array('resource_type' => 'Deployment', 'tags' => array('rs_guzzle_client:tag1=true')), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());
-		$json_obj = json_decode($result->getBody(true));
-		$this->assertEquals(1, count($json_obj));
-		$hasSearchedTag = false;
-		foreach($json_obj[0]->tags as $tag) {
-			if($tag->name == 'rs_guzzle_client:tag1=true') {
-				$hasSearchedTag = true;
-			}				
-		}
-		$this->assertTrue($hasSearchedTag);
-		
-		# Search by different resource type
-		$command = null;
-		$result = $this->executeCommand('tags_search', array('resource_type' => 'Server', 'tags' => array('rs_guzzle_client:tag1=true')), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());
-		$json_obj = json_decode($result->getBody(true));
-		$this->assertEquals(0, count($json_obj));
-		
-		# Search with match_all		
-		$command = null;
-		$result = $this->executeCommand('tags_search', array('resource_type' => 'Deployment', 'tags' => array('rs_guzzle_client:tag1=true','rs_guzzle_client:tagfoo=true'), 'match_all' => 'true'), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());
-		$json_obj = json_decode($result->getBody(true));
-		$this->assertEquals(0, count($json_obj));
-		
-		$deployment->destroy();
+	public function testCanSearchAsJson() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_search/js/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_search', array('output_format' => '.js'));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/tags/search.js', $request);
 	}
-	
+
 	/**
 	 * @group v1_0
-	 * @group integration
+	 * @group unit
 	 */
-	public function testCanSearchWithAllParamsXml() {		
-		$deployment = new Deployment();
-		$deployment->nickname = "Guzzle_Test_For_Tag_" . $this->_testTs;
-		$deployment->description = 'described';
-		$deployment->create();
-		
-		# Check for 0 tags first
-		$command = null;
-		$result = $this->executeCommand('tags_search', array('resource_href' => $deployment->href, 'output_format' => '.xml'), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());
-		$this->assertInstanceOf('SimpleXMLElement', $result);
-		$this->assertObjectNotHasAttribute('tag', $result);
-		
-		# Set one tag, then find it, this is duplicated above, but let's do it here for good measure
-		$result = $this->executeCommand('tags_set', array('resource_href' => $deployment->href, 'tags' => array('rs_guzzle_client:tag1=true')));
-		$this->assertEquals(204, $result->getStatusCode());
-		$command = null;
-		$result = $this->executeCommand('tags_search', array('resource_href' => $deployment->href, 'output_format' => '.xml'), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());
-		$this->assertInstanceOf('SimpleXMLElement', $result);
-		$this->assertObjectHasAttribute('tag', $result);		
-		$this->assertEquals(1, count($result->tag));
-		
-		# Set another tag, used for additional tests later
-		$result = $this->executeCommand('tags_set', array('resource_href' => $deployment->href, 'tags' => array('rs_guzzle_client:tag2=true')));
-		$this->assertEquals(204, $result->getStatusCode());
-		
-		# Search by deployment resource type
-		$command = null;
-		$result = $this->executeCommand('tags_search', array('resource_type' => 'Deployment', 'tags' => array('rs_guzzle_client:tag1=true'), 'output_format' => '.xml'), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());
-		$this->assertInstanceOf('SimpleXMLElement', $result);
-		$this->assertObjectHasAttribute('deployment', $result);		
-		$this->assertEquals(2, count($result->deployment->tags->tag));
-		$hasSearchedTag = false;
-		foreach($result->deployment->tags->tag as $tag) {
-			if($tag->name == 'rs_guzzle_client:tag1=true') {
-				$hasSearchedTag = true;
-			}				
-		}
-		$this->assertTrue($hasSearchedTag);
-		
-		# Search by different resource type
-		$command = null;
-		$result = $this->executeCommand('tags_search', array('resource_type' => 'Server', 'tags' => array('rs_guzzle_client:tag1=true'), 'output_format' => '.xml'), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());
-		$this->assertInstanceOf('SimpleXMLElement', $result);
-		$this->assertObjectNotHasAttribute('tag', $result);
-		
-		# Search with match_all		
-		$command = null;
-		$result = $this->executeCommand('tags_search', array('resource_type' => 'Deployment', 'tags' => array('rs_guzzle_client:tag1=true','rs_guzzle_client:tagfoo=true'), 'match_all' => 'true', 'output_format' => '.xml'), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());
-		$this->assertInstanceOf('SimpleXMLElement', $result);
-		$this->assertObjectNotHasAttribute('tag', $result);
-		
-		$deployment->destroy();
+	public function testCanSearchAsXml() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/tags_search/xml/response'
+      )
+    );
+
+    $command = $client->getCommand('tags_search', array('output_format' => '.xml'));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/tags/search.xml', $request);
 	}
 }
 
