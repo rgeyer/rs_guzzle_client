98182892ca0285c512f399904b547e9fb64714e5
Ryan J. Geyer <me@ryangeyer.com>
2012-09-11 11:43:59 -0700

    Refactored Ec2\SqsQueuesTest to proper unit tests

--- Tests/Command/Ec2/SqsQueuesTest.php
+++ Tests/Command/Ec2/SqsQueuesTest.php
@@ -1,54 +1,429 @@
 <?php
 namespace RGeyer\Guzzle\Rs\Tests\Command\Ec2;
 
-use RGeyer\Guzzle\Rs\Tests\Utils\ClientCommandsBase;
+use RGeyer\Guzzle\Rs\Common\ClientFactory;
 
-/**
- * TODO: Creation throws 422 so can't test anything.
- *
- */
-class SqsQueuesTest extends ClientCommandsBase {
-	
-	public static function setUpBeforeClass() {
-		
-	}
-	
-	public static function tearDownAfterClass() {
-		
-	}
-	
+class SqsQueuesTest extends \Guzzle\Tests\GuzzleTestCase {
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testHasIndexCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('sqs_queues');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testIndexUsesCorrectVerb() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queues/js/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queues');
+    $command->execute();
+
+    $this->assertEquals('GET', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testIndexCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('sqs_queues');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
 	/**
 	 * @group v1_0
-	 * @group integration
+	 * @group unit
 	 */
-	public function testCanCreateSqsQueue() {
-		$this->markTestSkipped("Even with correct parameters, this throws 422!");
-		$command = null;
-		$result = $this->executeCommand('sqs_queues_create',
-			array(
-				'sqs_queue[name]' => 'GT',
-				'sqs_queue[api_generation]' => 1
-			),
-			$command
-		);
-		
-		$this->assertEquals(201, $command->getResponse()->getStatusCode());
-		$this->assertNotNull($command->getResponse()->getHeader('Location'));
-		
-		$sqs_id = $this->getIdFromHref('sqs_queues', $command->getResponse()->getHeader('Location'));
-		
-		return $sqs_id;
+	public function testIndexDefaultsOutputTypeToJson() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queues/js/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queues');
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/sqs_queues.js', $request);
 	}
-	
+
 	/**
 	 * @group v1_0
-	 * @group integration
- 	 * @depends testCanCreateSqsQueue
+	 * @group unit
 	 */
-	public function testCanDestroySqsQueue($id) {
-		$command = null;
-		$result = $this->executeCommand('sqs_queues_destroy', array('id' => $id), $command);
-		$this->assertEquals(200, $command->getResponse()->getStatusCode());		
+	public function testCanRequestIndexAsJson() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queues/js/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queues', array('output_format' => '.js'));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/sqs_queues.js', $request);
 	}
+
+	/**
+	 * @group v1_0
+	 * @group unit
+	 */
+	public function testCanRequestIndexAsXml() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queues/xml/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queues', array('output_format' => '.xml'));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/sqs_queues.xml', $request);
+	}
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testHasShowCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('sqs_queue');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testShowUsesCorrectVerb() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queue/js/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queue', array('id' => 1234));
+    $command->execute();
+
+    $this->assertEquals('GET', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testShowCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('sqs_queue');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage  id argument be supplied.
+   */
+  public function testShowRequiresId() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queue/js/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queue');
+    $command->execute();
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage id: Value must be numeric
+   */
+  public function testShowRequiresIdToBeAnInt() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queue/js/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queue', array('id' => 'abc'));
+    $command->execute();
+  }
+
+	/**
+	 * @group v1_0
+	 * @group unit
+	 */
+	public function testShowDefaultsOutputTypeToJson() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queue/js/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queue', array('id' => 1234));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/sqs_queues/1234.js', $request);
+	}
+
+	/**
+	 * @group v1_0
+	 * @group unit
+	 */
+	public function testShowAsJson() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queue/js/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queue', array('id' => 1234, 'output_format' => '.js'));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/sqs_queues/1234.js', $request);
+	}
+
+	/**
+	 * @group v1_0
+	 * @group unit
+	 */
+	public function testShowAsXml() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queue/xml/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queue', array('id' => 1234, 'output_format' => '.xml'));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/sqs_queues/1234.xml', $request);
+	}
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testShowCommandReturnsAModel() {
+    $this->markTestSkipped("A model does not yet exist");
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queue/js/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queue', array('id' => '12345'));
+    $command->execute();
+    $result = $command->getResult();
+
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Model\Ec2\SqsQueue', $result);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testHasCreateCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('sqs_queues_create');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testCreateUsesCorrectVerb() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queues_create/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queues_create',
+      array(
+        'sqs_queue[name]' => 'name'
+      )
+    );
+    $command->execute();
+
+    $this->assertEquals('POST', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testCreateCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('sqs_queues_create');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage sqs_queue[name] argument be supplied.
+   */
+  public function testCreateRequiresNickname() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queues_create/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queues_create');
+    $command->execute();
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testCreateCommandReturnsAModel() {
+    $this->markTestSkipped("A model does not yet exist");
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queues_create/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queues_create',
+      array(
+        'sqs_queue[name]' => 'name'
+      )
+    );
+    $command->execute();
+    $result = $command->getResult();
+
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Model\Ec2\SqsQueue', $result);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testHasDestroyCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('sqs_queues_destroy');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testDestroyUsesCorrectVerb() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queues_destroy/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queues_destroy',array('id' => 1234));
+    $command->execute();
+
+    $this->assertEquals('DELETE', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   */
+  public function testDestroyCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient();
+    $command = $client->getCommand('sqs_queues_destroy');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage  id argument be supplied.
+   */
+  public function testDestroyRequiresId() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queues_destroy/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queues_destroy');
+    $command->execute();
+  }
+
+  /**
+   * @group v1_0
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage id: Value must be numeric
+   */
+  public function testDestroyRequiresIdToBeAnInt() {
+    $client = ClientFactory::getClient();
+    $this->setMockResponse($client,
+      array(
+        '1.0/login',
+        '1.0/sqs_queues_destroy/response'
+      )
+    );
+
+    $command = $client->getCommand('sqs_queues_destroy', array('id' => 'abc'));
+    $command->execute();
+  }
 	
 }
