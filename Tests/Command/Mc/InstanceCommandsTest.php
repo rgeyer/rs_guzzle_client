fdd5457e6f801e7ae14e3e101c7d981afcfb3705
Ryan J. Geyer <me@ryangeyer.com>
2012-10-03 16:53:04 -0700

    Added MC instance show and index commands

---
+++ Tests/Command/Mc/InstanceCommandsTest.php
@@ -0,0 +1,356 @@
+<?php
+namespace RGeyer\Guzzle\Rs\Tests\Command\Mc;
+
+use RGeyer\Guzzle\Rs\Common\ClientFactory;
+
+class InstanceCommandsTest extends \RGeyer\Guzzle\Rs\Tests\Utils\ClientCommandsBase {
+
+  /**
+   * @group v1_5
+   * @group unit
+   */
+  public function testHasIndexCommand() {
+    $client = ClientFactory::getClient('1.5');
+    $command = $client->getCommand('instances');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_5
+   * @group unit
+   */
+  public function testIndexUsesCorrectVerb() {
+    $client = ClientFactory::getClient('1.5');
+    $this->setMockResponse($client,
+      array(
+        '1.5/login',
+        '1.5/instances/json/response'
+      )
+    );
+
+    $command = $client->getCommand('instances', array('cloud_id' => '1234'));
+    $command->execute();
+
+    $this->assertEquals('GET', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_5
+   * @group unit
+   */
+  public function testIndexCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient('1.5');
+    $command = $client->getCommand('instances');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
+	/**
+	 * @group v1_5
+	 * @group unit
+	 */
+	public function testIndexDefaultsOutputTypeToJson() {
+    $client = ClientFactory::getClient('1.5');
+    $this->setMockResponse($client,
+      array(
+        '1.5/login',
+        '1.5/instances/json/response'
+      )
+    );
+
+    $command = $client->getCommand('instances', array('cloud_id' => '1234'));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/api/clouds/1234/instances.json', $request);
+	}
+
+	/**
+	 * @group v1_5
+	 * @group unit
+	 */
+	public function testCanRequestIndexAsJson() {
+    $client = ClientFactory::getClient('1.5');
+    $this->setMockResponse($client,
+      array(
+        '1.5/login',
+        '1.5/instances/json/response'
+      )
+    );
+
+    $command = $client->getCommand('instances', array('cloud_id' => '1234', 'output_format' => '.json'));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/api/clouds/1234/instances.json', $request);
+	}
+
+	/**
+	 * @group v1_5
+	 * @group unit
+	 */
+	public function testCanRequestIndexAsXml() {
+    $client = ClientFactory::getClient('1.5');
+    $this->setMockResponse($client,
+      array(
+        '1.5/login',
+        '1.5/instances/xml/response'
+      )
+    );
+
+    $command = $client->getCommand('instances', array('cloud_id' => '1234', 'output_format' => '.xml'));
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/api/clouds/1234/instances.xml', $request);
+	}
+
+  /**
+   * @group v1_5
+   * @group unit
+   */
+  public function testIndexAcceptsFilters() {
+    $client = ClientFactory::getClient('1.5');
+    $command = $client->getCommand('instances');
+    $args = $command->getApiCommand()->getParams();
+    $filter_param = array_filter(
+      $args,
+      function($arg) {
+        return $arg->getName() == 'filter';
+      }
+    );
+
+    $this->assertNotNull($filter_param);
+    $this->assertEquals(1, count($filter_param));
+  }
+
+  /**
+   * @group v1_5
+   * @group unit
+   */
+  public function testIndexAcceptsViews() {
+    $client = ClientFactory::getClient('1.5');
+    $command = $client->getCommand('instances');
+    $args = $command->getApiCommand()->getParams();
+    $filter_param = array_filter(
+      $args,
+      function($arg) {
+        return $arg->getName() == 'view';
+      }
+    );
+
+    $this->assertNotNull($filter_param);
+    $this->assertEquals(1, count($filter_param));
+  }
+
+  /**
+   * @group v1_5
+   * @group unit
+   */
+  public function testHasShowCommand() {
+    $client = ClientFactory::getClient('1.5');
+    $command = $client->getCommand('instance');
+    $this->assertNotNull($command);
+  }
+
+  /**
+   * @group v1_5
+   * @group unit
+   */
+  public function testShowUsesCorrectVerb() {
+    $client = ClientFactory::getClient('1.5');
+    $this->setMockResponse($client,
+      array(
+        '1.5/login',
+        '1.5/instance/json/response'
+      )
+    );
+
+    $command = $client->getCommand('instance',
+      array(
+        'id' => '1234',
+        'cloud_id' => '1234'
+      )
+    );
+    $command->execute();
+
+    $this->assertEquals('GET', $command->getRequest()->getMethod());
+  }
+
+  /**
+   * @group v1_5
+   * @group unit
+   */
+  public function testShowCommandExtendsDefaultCommand() {
+    $client = ClientFactory::getClient('1.5');
+    $command = $client->getCommand('instance');
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Command\DefaultCommand', $command);
+  }
+
+  /**
+   * @group v1_5
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage cloud_id argument be supplied.
+   */
+  public function testShowRequiresCloudId() {
+    $client = ClientFactory::getClient('1.5');
+    $this->setMockResponse($client,
+      array(
+        '1.5/login',
+        '1.5/instance/json/response'
+      )
+    );
+
+    $command = $client->getCommand('instance',
+      array(
+        'id' => '1234'
+      )
+    );
+    $command->execute();
+  }
+
+  /**
+   * @group v1_5
+   * @group unit
+   * @expectedException Guzzle\Service\Exception\ValidationException
+   * @expectedExceptionMessage  id argument be supplied.
+   */
+  public function testShowRequiresId() {
+    $client = ClientFactory::getClient('1.5');
+    $this->setMockResponse($client,
+      array(
+        '1.5/login',
+        '1.5/server/json/response'
+      )
+    );
+
+    $command = $client->getCommand('instance',
+      array(
+        'cloud_id' => '1234'
+      )
+    );
+    $command->execute();
+  }
+
+	/**
+	 * @group v1_5
+	 * @group unit
+	 */
+	public function testShowDefaultsOutputTypeToJson() {
+    $client = ClientFactory::getClient('1.5');
+    $this->setMockResponse($client,
+      array(
+        '1.5/login',
+        '1.5/instance/json/response'
+      )
+    );
+
+    $command = $client->getCommand('instance',
+      array(
+        'id' => '1234',
+        'cloud_id' => '1234'
+      )
+    );
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/api/clouds/1234/instances/1234.json', $request);
+	}
+
+	/**
+	 * @group v1_5
+	 * @group unit
+	 */
+	public function testShowAsJson() {
+    $client = ClientFactory::getClient('1.5');
+    $this->setMockResponse($client,
+      array(
+        '1.5/login',
+        '1.5/instance/json/response'
+      )
+    );
+
+    $command = $client->getCommand('instance',
+      array(
+        'id' => '1234',
+        'cloud_id' => '1234',
+        'output_format' => '.json'
+      )
+    );
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/api/clouds/1234/instances/1234.json', $request);
+	}
+
+	/**
+	 * @group v1_5
+	 * @group unit
+	 */
+	public function testShowAsXml() {
+    $client = ClientFactory::getClient('1.5');
+    $this->setMockResponse($client,
+      array(
+        '1.5/login',
+        '1.5/instance/xml/response'
+      )
+    );
+
+    $command = $client->getCommand('instance',
+      array(
+        'id' => '1234',
+        'cloud_id' => '1234',
+        'output_format' => '.xml'
+      )
+    );
+    $command->execute();
+
+    $request = (string)$command->getRequest();
+    $this->assertContains('/api/clouds/1234/instances/1234.xml', $request);
+	}
+
+  /**
+   * @group v1_5
+   * @group unit
+   */
+  public function testShowAcceptsViews() {
+    $client = ClientFactory::getClient('1.5');
+    $command = $client->getCommand('instance');
+    $args = $command->getApiCommand()->getParams();
+    $filter_param = array_filter(
+      $args,
+      function($arg) {
+        return $arg->getName() == 'view';
+      }
+    );
+
+    $this->assertNotNull($filter_param);
+    $this->assertEquals(1, count($filter_param));
+  }
+
+  /**
+   * @group v1_5
+   * @group unit
+   */
+  public function testShowCommandReturnsAModel() {
+    $this->markTestSkipped("A model does not yet exist");
+    $client = ClientFactory::getClient('1.5');
+    $this->setMockResponse($client,
+      array(
+        '1.5/login',
+        '1.5/instance/json/response'
+      )
+    );
+
+    $command = $client->getCommand('instance',
+      array(
+        'id' => '1234',
+        'cloud_id' => '1234'
+      )
+    );
+    $command->execute();
+    $result = $command->getResult();
+
+    $this->assertInstanceOf('RGeyer\Guzzle\Rs\Model\Mc\Instance', $result);
+  }
+}
