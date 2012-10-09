88f04edee1cb50ade6bca6af0a6b07d50b630dac
Ryan J. Geyer <me@ryangeyer.com>
2012-10-09 16:47:51 -0700

    Added some testing of parsing to Server Model

--- Tests/Model/Mc/ServerTest.php
+++ Tests/Model/Mc/ServerTest.php
@@ -3,11 +3,30 @@
 namespace RGeyer\Guzzle\Rs\Test\Model\Mc;
 
 use RGeyer\Guzzle\Rs\Model\Mc\Server;
+use RGeyer\Guzzle\Rs\Common\ClientFactory;
 
 class ServerTest extends \Guzzle\Tests\GuzzleTestCase {
 
+	protected function setUp() {
+		parent::setUp();
+
+		$this->setMockResponse(ClientFactory::getClient('1.5'), '1.5/login');
+		ClientFactory::getClient('1.5')->get('session')->send();
+	}
+
+	/**
+	 * @group v1_5
+	 * @group unit
+	 */
+	public function testCanParseJsonResponse() {
+		$this->setMockResponse(ClientFactory::getClient('1.5'), '1.5/server/json/response');
+		$server = new Server();
+    $server->find_by_id('12345');
+    $this->assertEquals($server->id, '12345');
+	}
+
   /**
-   * @group v1_0
+   * @group v1_5
    * @group unit
    */
   public function testExtendsAbstractServer() {
